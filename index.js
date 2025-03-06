aa7e086e1d9d0514ccd77173133bbfa717578f2c
André Mendes <andre@mendesc.com>
2025-03-06 11:41:55 -0300

    solution to licence key formatting problem

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ Output: “2-5G-3J”
 
 Explanation: The string s has been split into three parts, each part has 2 characters except the first part, as it could be shorter as mentioned above
 */
+
+const licenceKeyFormat = (str, k) => {
+    const output = [];
+    let currAmount = 0;
+    for(let i=str.length - 1; i >= 0; i--) {
+        if(str[i] !== '-') {
+            output.push(str[i].toUpperCase());
+            ++currAmount;
+            if(currAmount === k && i !== 0) {
+                output.push('-');
+                currAmount = 0;
+            } 
+        }
+    }
+
+    return output.reverse().join("");
+}
+
+console.log(licenceKeyFormat("2-5g-3-J", 2));
+console.log(licenceKeyFormat("5F3Z-2E9W", 4));
+console.log(licenceKeyFormat("33223-2-5F3Z-2E9W", 3));
